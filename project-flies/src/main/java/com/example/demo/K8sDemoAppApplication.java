75e03d380491965dc290b192452ecaf310f9d8b1
everythingeverywhere <ivan0tarin@gmail.com>
2021-03-01 08:42:53 -0700

    began making project folder

--- project-flies/src/main/java/com/example/demo/K8sDemoAppApplication.java
+++ project-flies/src/main/java/com/example/demo/K8sDemoAppApplication.java
@@ -2,20 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@RestController
 public class K8sDemoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(K8sDemoAppApplication.class, args);
 	}
 
-@GetMapping("/")
-public String hello() {
-    return "Hello World";
-}
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
 }
